8b504339ff90334dd522a2413f6ff5df5a9fbf0e
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-05 21:02:03 +0200

    added some code on using variables and the variables exercise

---
+++ javascript/variables/variables.js
@@ -0,0 +1,13 @@
+// This is some new code I learned ie prompt() note hit shift enter to enter more than 1 line in the console 
+// to be run in the browser console
+
+// VARIABLES - VAR
+var numberOne = prompt("Please enter the first number");
+var numberTwo = prompt("Please enter the second number");
+
+// since prompt brings in the inputs as strings we need to convert them to numbers
+
+var sum = Number(numberOne) + Number(numberTwo);
+
+alert("Your Sum is " + sum);
+
