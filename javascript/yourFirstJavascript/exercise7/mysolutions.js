78dd32a8482368be2ac3a28b5ec18acb6b8b2326
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-08 23:36:20 +0200

    added my solution for exercise 7 on objects and arrays

---
+++ javascript/yourFirstJavascript/exercise7/mysolutions.js
@@ -0,0 +1,21 @@
+var user = {
+    name:"Ayesha",
+    password:"test",
+}
+
+var database = [user];
+
+var newsFeed = [
+    {
+        username:"ayesha",
+        timeline:"something"
+    },
+    {
+        username:"ayesha2",
+        timeline:"something2"
+    },
+    {
+        username:"ayesha3",
+        timeline:"something3"
+    },
+]
