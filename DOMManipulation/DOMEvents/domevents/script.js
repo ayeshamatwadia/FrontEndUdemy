21cf9244e4563f33e6cc6b8068587337be8f8084
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-11 22:18:02 +0200

    added my solution to the dom manipulation todolist exercise

--- DOMManipulation/DOMEvents/domevents/script.js
+++ DOMManipulation/DOMEvents/domevents/script.js
@@ -7,9 +7,8 @@ function inputLength() {
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
+	ul.appendChild(createNewListItem(input));
+	ul.appendChild(createButton());
 	input.value = "";
 }
 
@@ -28,3 +27,41 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+// My Solution to the exercise
+
+var ul = document.querySelector("ul");
+
+ul.addEventListener("click", function(e){
+	if(e.target.tagName === "LI"){
+		e.target.classList.toggle("done");
+	}	
+})
+
+function createButton() {
+	var button = document.createElement("button");
+	button.appendChild(document.createTextNode("DELETE"));
+	button.addEventListener("click", deleteFunction);
+	return button;
+  };
+
+  function createNewListItem (input) {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	return li;
+  };
+
+  var deleteButtons = document.getElementsByClassName("delete");
+
+//   deleteButtons.array.forEach(element => {
+// 	  element.addEventListener("click", deleteFunction);
+//   });
+
+  for(var i=0; i< deleteButtons.length; i++){
+	deleteButtons[i].addEventListener("click", deleteFunction);
+  }
+
+  function deleteFunction(e) {
+	ul.removeChild(e.target.previousElementSibling);
+	ul.removeChild(e.target);
+  }
