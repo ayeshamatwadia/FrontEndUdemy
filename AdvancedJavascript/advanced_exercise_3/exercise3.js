f022446d8d24da006cf5092b6b0ea2360d9cf3f2
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-17 22:21:27 +0200

    added my solution to the new javascript syntax examples

--- AdvancedJavascript/advanced_exercise_3/exercise3.js
+++ AdvancedJavascript/advanced_exercise_3/exercise3.js
@@ -1,9 +1,9 @@
 // change everything below to the newer Javascript!
 
 // let + const
-var a = 'test';
-var b = true;
-var c = 789;
+let a = 'test';
+const b = true;
+const c = 789;
 a = 'test2';
 
 
@@ -15,36 +15,39 @@ var person = {
     eyeColor  : "blue"
 };
 
-var firstName = person.firstName;
-var lastName = person.lastName;
-var age = person.age;
-var eyeColor = person.eyeColor;
+// var firstName = person.firstName;
+// var lastName = person.lastName;
+// var age = person.age;
+// var eyeColor = person.eyeColor;
 
+let {firstName, lasName, age, eyeColor} = person
 
 // Object properties
-var a = 'test';
-var b = true;
-var c = 789;
+let a = 'test';
+let b = true;
+let c = 789;
 
 var okObj = {
-  a: a,
-  b: b,
-  c: c
+ a,
+ b,
+ c
 };
 
 
 // Template strings
 var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
-
+var messageNew = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
 
 // default arguments
 // default age to 10;
-function isValidAge(age) {
+function isValidAge(age=10) {
     return age
 }
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
+
+let sym = Symbol("test");
 
 // Arrow functions
 function whereAmI(username, location) {
@@ -54,3 +57,5 @@ function whereAmI(username, location) {
         return "I am totally lost!";
     }
 }
+
+const fun = (username, location) => (username && location) ? "I am not lost" : "I am totally lost!";
