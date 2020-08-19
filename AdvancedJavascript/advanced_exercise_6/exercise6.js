5fe70267fe82e267033532dcd9ed5e43ff2428db
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-19 21:23:38 +0200

    added my solution for the exercise

--- AdvancedJavascript/advanced_exercise_6/exercise6.js
+++ AdvancedJavascript/advanced_exercise_6/exercise6.js
@@ -1,7 +1,9 @@
 //Evaluate these:
 //#1
-[2] === [2] 
-{} === {} 
+[2] === [2]
+false
+{} === {}
+false 
 
 //#2 what is the value of property a for each object.
 const object1 = { a: 5 }; 
@@ -10,6 +12,30 @@ const object3 = object2;
 const object4 = { a: 5}; 
 object1.a = 4;
 
+// object 1 a:4 object2 a:4 object3 a:4 object4 a:5
+
 
 //#3 create two classes: an Animal class and a Mamal class. 
-// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+// create a cow that accepts a name, type and 
+// color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+    constructor(name, type, color){
+        this.name = name;
+        this.type = type;
+        this.color = color
+    }
+     moo() {
+        console.log(`My name is ${this.name} my type is ${this.type} and my color is ${this.color}`);
+    }
+}
+
+class Mamal extends Animal {
+    constructor(name, type, color){
+        super(name, type, color);
+    }
+}
+
+let cow = new Mamal("daisy", "Kobe", "brown");
+
+cow.moo();
