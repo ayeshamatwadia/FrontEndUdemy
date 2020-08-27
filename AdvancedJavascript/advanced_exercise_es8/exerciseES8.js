2826cec92dec9c9fff8d3593776c787cfd31e5fe
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-08-27 22:27:10 +0200

    added my solutions for ES8

--- AdvancedJavascript/advanced_exercise_es8/exerciseES8.js
+++ AdvancedJavascript/advanced_exercise_es8/exerciseES8.js
@@ -9,8 +9,10 @@ let rabbit = '🐇';
 '     ||<- Start line'
 '       🐢'
 '       🐇'
-
+turtle.padStart(7);
+rabbit.padStart(7);
 // when you do:
+
 console.log(startLine);
 console.log(turtle);
 console.log(rabbit);
@@ -19,6 +21,7 @@ console.log(rabbit);
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
+// adds 7 =s to the end of the turtle
 
 
 // #3) Get the below object to go from:
@@ -27,5 +30,15 @@ let obj = {
   is: 'Rudolf',
   the: 'raindeer'
 }
+
+let finishString ="";
+
+Object.entries(obj).map(arr => {
+  finishString = `${finishString} ${arr[0]} ${arr[1]}`;
+})
+
 // to this:
 'my name is Rudolf the raindeer'
+
+// answer in the solutions
+Object.entries(obj).map(value => value.join(" ")).join(' ')
