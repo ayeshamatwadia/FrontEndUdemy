12e982618ff09f69bce8edcd002f86ab2d8fc8cf
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2020-09-09 22:17:35 +0200

    added my solution to the promises exercise

--- Promises/exercise_promises/exercisePromises.js
+++ Promises/exercise_promises/exercisePromises.js
@@ -1,16 +1,27 @@
 // Solve the questions below:
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
+const fourSecondPromise = new Promise((resolve, reject) => {
+  setTimeout(resolve, 400, "success");
+})
+// incorrect see answers
 
 // #2) Run the above promise and make it console.log "success"
+fourSecondPromise.then(result => console.log(result));
 
 
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
 // the above promise shorter with Promise.resolve() and console loggin "success"
+Promise.resolve("success").then(function(value){
+  console.log(value);
+})
 
+// this is incorrect as well
 
 // #4) Catch this error and console log 'Ooops something went wrong'
-Promise.reject('failed')
+Promise.reject('failed').catch(() =>{
+  console.log("Oooops something went wrong")
+})
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
@@ -21,5 +32,12 @@ const urls = [
   'http://swapi.dev/api/people/4'
 ]
 
+Promise.all(urls).then((values) => {
+  console.log(values);
+}).catch(()=> console.log("there was an error"))
+
+// this is also incorrect - you forgot to make the call
+
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
+// 
